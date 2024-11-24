ea178310955ffa93dbdd90be6f7a689c9e709cab
Inês Rocha <115479617+rocha1202@users.noreply.github.com>
2024-11-24 20:38:22 +0000

    Revert "cores fonte e estilização navbar"
    This reverts commit ca432a2a5d7ffee9d7a7a1e8bf8f2d86a11a57b4.

--- artFusion/src/main.js
+++ artFusion/src/main.js
@@ -1,26 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
 
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.min.css'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
 const app = createApp(App)
+const pinia = createPinia();
 
-app.use(createPinia())
 app.use(router)
-app.use(vuetify)
+app.use(pinia);
+
 
 app.mount('#app')
