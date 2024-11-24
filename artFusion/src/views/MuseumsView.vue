df242e0c9d996d6a18d9dab05d6d3cc394319528
Beatriz Costa <120328207+xbeatriz@users.noreply.github.com>
2024-11-24 11:36:28 +0000

    event add to museum view

--- artFusion/src/views/MuseumsView.vue
+++ artFusion/src/views/MuseumsView.vue
@@ -1,12 +1,27 @@
 <template>
+  <div>
+    <h3>Museums</h3>
     <div>
-      <h3>Museums</h3>
+      <EventCard
+        imageSrc="https://via.placeholder.com/300x200"
+        title="Event Title"
+        info="this is the info of the card"
+        buttonText="Join Now"
+      >
+      </EventCard>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: 'Museums',
-  };
-  </script>
-  
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "Museums",
+  components: {
+    EventCard,
+  },
+};
+</script>
+
+ 
